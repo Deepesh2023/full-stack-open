7434acb94114130e3d84c191aee2523d44344455
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-03-25 12:01:16 +0530

    coures information, step 2

--- part_1/courseInfo/src/App.jsx
+++ part_1/courseInfo/src/App.jsx
@@ -10,9 +10,14 @@ const App = () => {
   return (
     <>
       <Header course={course} />
-      <Content part_number={part1} exercises={exercises1} />
-      <Content part_number={part2} exercises={exercises2} />
-      <Content part_number={part3} exercises={exercises3} />
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
       <Total total={exercises1 + exercises2 + exercises3} />
     </>
   );
@@ -29,8 +34,18 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <>
       <p>
-        {props.part_number} {props.exercises}
+        {props.part} {props.exercises}
       </p>
     </>
   );
