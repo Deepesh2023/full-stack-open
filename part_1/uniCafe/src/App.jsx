e9af9298a6b39e2da90a8a28674569f9347d56ab
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-04-04 09:46:53 +0530

    unicafe, step 1 refactored

--- part_1/uniCafe/src/App.jsx
+++ part_1/uniCafe/src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from "react";
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [statCategory, setStat] = useState({ good: 0, neutral: 0, bad: 0 });
 
-  const goodButtonAction = () => {
-    setGood(good + 1);
-  };
+  const buttonAction = (selectedStat) => {
+    const updatedStat = statCategory[selectedStat] + 1;
 
-  const neutralButtonAction = () => {
-    setNeutral(neutral + 1);
-  };
+    const updatedStats = {
+      ...statCategory,
+      [selectedStat]: updatedStat,
+    };
 
-  const badButtonAction = () => {
-    setBad(bad + 1);
+    return () => setStat(updatedStats);
   };
 
   return (
     <div>
       <Heading heading="give feedback" />
-      <Button buttonName="Good" buttonAction={goodButtonAction} />
-      <Button buttonName="Neutral" buttonAction={neutralButtonAction} />
-      <Button buttonName="Bad" buttonAction={badButtonAction} />
+      <Button buttonName="Good" buttonAction={buttonAction("good")} />
+      <Button buttonName="Neutral" buttonAction={buttonAction("neutral")} />
+      <Button buttonName="Bad" buttonAction={buttonAction("bad")} />
       <Heading heading="statistics" />
-      <Statistics statsName="Good" stats={good} />
-      <Statistics statsName="Neutral" stats={neutral} />
-      <Statistics statsName="Bad" stats={bad} />
+      <Statistics statsName="Good" stats={statCategory.good} />
+      <Statistics statsName="Neutral" stats={statCategory.neutral} />
+      <Statistics statsName="Bad" stats={statCategory.bad} />
     </div>
   );
 };
