3ed07a6196238fd2dc491adb6610fd5c0bd4fe4c
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-04-04 17:42:16 +0530

    Unicafe, step 2

--- part_1/uniCafe/src/App.jsx
+++ part_1/uniCafe/src/App.jsx
@@ -1,17 +1,37 @@
 import { useState } from "react";
 
 const App = () => {
-  const [statCategory, setStat] = useState({ good: 0, neutral: 0, bad: 0 });
+  const [statCategory, setStats] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    total: 0,
+    average: 0,
+    positivePercentage: 0,
+  });
 
   const buttonAction = (selectedStat) => {
-    const updatedStat = statCategory[selectedStat] + 1;
+    const updatedSelectedStat = statCategory[selectedStat] + 1;
+    const updatedTotal = statCategory.total + 1;
 
     const updatedStats = {
       ...statCategory,
-      [selectedStat]: updatedStat,
+      [selectedStat]: updatedSelectedStat,
+      total: updatedTotal,
     };
 
-    return () => setStat(updatedStats);
+    const updatedAverage =
+      (updatedStats.good + -updatedStats.bad) / updatedStats.total;
+
+    const updatedPositivePercentage =
+      (updatedStats.good / updatedStats.total) * 100;
+
+    updatedStats.average = updatedAverage;
+    updatedStats.positivePercentage = updatedPositivePercentage;
+
+    return () => {
+      setStats(updatedStats);
+    };
   };
 
   return (
@@ -24,6 +44,12 @@ const App = () => {
       <Statistics statsName="Good" stats={statCategory.good} />
       <Statistics statsName="Neutral" stats={statCategory.neutral} />
       <Statistics statsName="Bad" stats={statCategory.bad} />
+      <Statistics statsName="All" stats={statCategory.total} />
+      <Statistics statsName="Average" stats={statCategory.average} />
+      <Statistics
+        statsName="Positive"
+        stats={statCategory.positivePercentage + " %"}
+      />
     </div>
   );
 };
