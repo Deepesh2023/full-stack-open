2ec2157d0e2c811fd35fabc03201e04199fbc0c3
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-04-11 05:02:25 +0530

    phonebook, step 1

---
+++ part_2/phonebook/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import ContactDisplay from "./ContactDisplay";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addNewNameButtonAction = (event) => {
+    event.preventDefault();
+
+    if (newName === "") {
+      return;
+    }
+
+    const newPersonObject = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(newPersonObject));
+    setNewName("");
+  };
+
+  const newNameDisplay = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewNameButtonAction}>
+        <div>
+          name: <input value={newName} onChange={newNameDisplay} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <ContactDisplay key={person.name} name={person.name} />
+        ))}
+      </ul>
+      ...
+    </div>
+  );
+};
+
+export default App;
---
+++ part_2/phonebook/src/ContactDisplay.jsx
@@ -0,0 +1,5 @@
+const ContactDisplay = ({ name }) => {
+  return <li>{name}</li>;
+};
+
+export default ContactDisplay;
