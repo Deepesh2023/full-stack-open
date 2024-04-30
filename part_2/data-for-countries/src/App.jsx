2c83f1034bcf5f7bba5103312709226588492a86
Deepesh2023 <officialdeepeshsg@gmail.com>
2024-04-30 13:50:22 +0530

    data for countries, step-1, completed the search function

--- part_2/data-for-countries/src/App.jsx
+++ part_2/data-for-countries/src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from "react";
+import countriesDataServices from "./services/countriesData";
 
-const App = () => {
+const App = ({ countryNames }) => {
   const [searchText, setSearchText] = useState("");
-  const [countriesFound, setCountriesFound] = useState([]);
+  const [countriesFound, setCountriesFound] = useState(null);
+
+  console.log(countriesFound);
 
   const searchTextOnChange = (event) => {
-    setSearchText(event.target.value);
+    const text = event.target.value;
+    setSearchText(text);
+    findCountry(text.toLowerCase());
+  };
+
+  const findCountry = (text) => {
+    console.log(text);
+    setCountriesFound(
+      countryNames.filter((countryName) =>
+        countryName.toLowerCase().includes(text)
+      )
+    );
   };
 
   return (
@@ -14,8 +28,22 @@ const App = () => {
         <label>Search for countries: </label>
         <input type="text" value={searchText} onChange={searchTextOnChange} />
       </form>
+      <FoundCountriesList countries={countriesFound} />
     </>
   );
+};
+
+const FoundCountriesList = ({ countries }) => {
+  if (countries) {
+    return (
+      <ul>
+        {countries.map((country) => (
+          <li key={country}>{country}</li>
+        ))}
+      </ul>
+    );
+  }
+  return null;
 };
 
 export default App;
--- part_2/data-for-countries/src/main.jsx
+++ part_2/data-for-countries/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import countriesDataServices from "./services/countriesData.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const countryNames = [];
+countriesDataServices.getAllNames().then((countries) => {
+  countries.forEach((country) => {
+    countryNames.push(country.name.common);
+  });
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <App countryNames={countryNames} />
+  </React.StrictMode>
+);
